73d23289645666e17c778ed554a2abcc069b7147
Edomina <genetedom10@gmail.com>
2022-03-10 13:52:44 -0800

    commit 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Description: main - prints out a string to stdout.
+ *
+ *   Return: 0 if sucess.
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
