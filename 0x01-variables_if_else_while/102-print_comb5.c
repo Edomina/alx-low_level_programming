c703ed375a93fbb0789837e833e8051e05645c28
Edomina <genetedom10@gmail.com>
2022-03-11 14:07:15 -0800

    commit ad 3

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints all possible combinations
+ * of two two-digits numbers.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int j, i;
+
+	for (j = 0; j <= 99; j++)
+	{
+		for (i = j; i <= 99; i++)
+		{
+			if (i != j)
+			{
+				putchar(j / 10 + 48);
+				putchar(j % 10 + 48);
+				putchar(' ');
+				putchar(i / 10 + 48);
+				putchar(i % 10 + 48);
+
+				if (j * 100 + i != 9899)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
