f2dd4293bf0eb358d46213ffc9e82e842085a4fe
Edomina <genetedom10@gmail.com>
2022-03-11 13:29:37 -0800

    commit 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints possible comninations of
+ * single-digit numbers
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
