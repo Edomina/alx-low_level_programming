f560b77fff4d05aecc25cb072dd2ca25b3678a0b
Edomina <genetedom10@gmail.com>
2022-03-11 11:19:20 -0800

    commited

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * Description: main - print if the number is negative or postive and equals with zero
+ * main - print if the number is negative or postive and equals with zero
  *
  * Return: Always 0 (Sucess)
  */
@@ -14,15 +14,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-	        printf("%i is positive\n", n);
+	      printf("%i is positive\n", n);
 	}
 	else if (n < 0)
 	{
-	        printf("%i is negative\n", n);
+	      printf("%i is negative\n", n);
         }
 	else
 	{
-	        printf("%i is zero\n", n);
+	      printf("%i is zero\n", n);
 	}
 	return (0);
 }
