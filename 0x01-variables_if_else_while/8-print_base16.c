f1c9bb68789e3824f23853df397e906409a5a8a1
Edomina <genetedom10@gmail.com>
2022-03-11 13:13:38 -0800

    commit 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints hexadecimals in lowercase
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 48; a <= 122; a++)
+	{
+
+		if((a < 58 || a >= 97) && a <= 102)
+		{
+			putchar(a);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
