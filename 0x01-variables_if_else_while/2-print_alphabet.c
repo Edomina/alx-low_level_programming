6407b47668cfb122ede318755a58fff648e0ab97
Edomina <genetedom10@gmail.com>
2022-03-11 12:21:20 -0800

    commit 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints the alphabet in lowercase,
+ * and then in uppercase, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'Z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+
+	putchar('\n');
+	return (0);
+}
