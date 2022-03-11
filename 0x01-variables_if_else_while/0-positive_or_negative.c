27c0e3acff41eb641268b9e3f988941ddb993e57
Edomina <genetedom10@gmail.com>
2022-03-11 11:15:42 -0800

    commited

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-	  printf("%i is postive\n ", n);
+	        printf("%i is positive\n", n);
 	}
-	  else if (n < 0)
-	  {
-	     printf("%i is negative\n", n);
-	  }
-	  else
-	  {
-	     printf("%i is zero\n", n);
+	else if (n < 0)
+	{
+	        printf("%i is negative\n", n);
+        }
+	else
+	{
+	        printf("%i is zero\n", n);
 	}
 	return (0);
 }
