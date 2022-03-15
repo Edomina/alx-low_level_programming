bc1c421341a495bba097ef8a6d2081cf7d2c2bb3
Edomina <genetedom10@gmail.com>
2022-03-15 12:25:15 -0700

    m

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,19 @@
+/*
+ * File: 1-alphabet.c
+ * Auth: tsega
+ */
+
 #include "main.h"
+
 /**
- * print_alphabet - prints alphabet in lowercase
- *
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
 void print_alphabet(void)
 {
-	char c;
+	char letter;
 
-	for (c = 'a'; c <= 'z'; c++)
-		_putchar(c);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
 	_putchar('\n');
 }
