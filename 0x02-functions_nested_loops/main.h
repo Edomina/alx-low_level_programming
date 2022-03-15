abdc25e2793a763c6e21ebd8ee663c90c6f83317
Edomina <99475092+Edomina@users.noreply.github.com>
2022-03-15 23:17:18 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: tsega
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x02-functions_nested_loops directory.
+ */
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -13,4 +21,6 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
 #endif
+© 2022 GitHub, Inc.
