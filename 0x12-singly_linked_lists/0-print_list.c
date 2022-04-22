267f006dd93c257b80460735f9ad8de93046fe6c
Edomina <genetedom10@gmail.com>
2022-04-22 10:57:07 -0700

    singly linked 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,26 +2,24 @@
 #include "lists.h"
 
 /**
- *print_list-print list of data
- *@h:list_t
- *Return:printvale of str
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
+ *
+ * Return: the number of nodes printed
  */
-
 size_t print_list(const list_t *h)
 {
-size_t s = 1;
-if (h == NULL)
-return (0);
-while (h->next != NULL)
-{
-if (h->str == NULL)
+	size_t n = 0;
 
-printf("[0] (nil)\n");
-else
-printf("[%u] %s\n", h->len, h->str);
-h = h->next;
-s++;
-}
-printf("[%u] %s\n", h->len, h->str);
-return (s);
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		n++;
+	}
+
+	return (n);
 }
